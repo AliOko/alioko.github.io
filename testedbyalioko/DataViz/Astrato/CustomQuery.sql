118f7d29ea3bc5e5a4058540154cc92225541263
AliOko <AliOko@users.noreply.github.com>
2021-12-14 20:43:50 +0100

    Update CustomQuery.sql

--- testedbyalioko/DataViz/Astrato/CustomQuery.sql
+++ testedbyalioko/DataViz/Astrato/CustomQuery.sql
@@ -1,8 +1,12 @@
-SELECT * FROM (SELECT TITLE, SERIES, BOOKINSERIES,
+-- Universe
+
+SELECT Title, Series, Bookinseries,
    CASE 
-       WHEN SERIES in ('Stormlight Archive','Mistborn') THEN 'Cosmere'
-       WHEN SERIES in ('The Skyward Series', 'Skyward Flight') THEN 'Cytoverse'
+       WHEN Series in ('Stormlight Archive','Mistborn') THEN 'Cosmere'
+       WHEN Series in ('The Skyward Series', 'Skyward Flight') THEN 'Cytoverse'
        ELSE 'Other'
     END universe
 FROM SIMPLE_DATASETS.SANDERSONBOOKS
-ORDER BY SERIES DESC, BOOKINSERIES ASC)
+ORDER BY Series DESC, Bookinseries ASC;
+
+--
